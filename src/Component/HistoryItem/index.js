8722d7856c30012659d84bdd3ce14bfc240ee86f
NRNagaphanideep <phanideep14@gmail.com>
2023-01-21 05:50:38 +0000

    testid error was coming

--- src/Component/HistoryItem/index.js
+++ src/Component/HistoryItem/index.js
@@ -6,21 +6,23 @@ const HistoryItem = props => {
   const onDeleteItem = () => {
     onDeleteHistory(id)
   }
-
   return (
     <li className="history">
-      <p className="title-stamp">{timeAccessed}</p>
+      <p className="time">{timeAccessed}</p>
       <div className="domain-details-container">
         <img src={logoUrl} alt="domain logo" className="domain-logo" />
         <p className="title">{title}</p>
         <p className="domain-url">{domainUrl}</p>
       </div>
-      <button type="button" className="delete-button" onClick={onDeleteItem}>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
-          alt="delete"
-        />
-      </button>
+      <div className="button-container">
+        <button className="delete-button" onClick={onDeleteItem} type="button">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+            className="delete-icon"
+          />
+        </button>
+      </div>
     </li>
   )
 }
