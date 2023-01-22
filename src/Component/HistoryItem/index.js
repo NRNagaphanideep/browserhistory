76a3edcc358fa9488e1cfd52f2aca96441b7634f
Nagaphanideep <95171605+NRNagaphanideep@users.noreply.github.com>
2023-01-22 07:09:20 +0530

    Update index.js

--- src/Component/HistoryItem/index.js
+++ src/Component/HistoryItem/index.js
@@ -3,19 +3,28 @@ import './index.css'
 const HistoryItem = props => {
   const {historyDetails, onDeleteHistory} = props
   const {id, timeAccessed, logoUrl, title, domainUrl} = historyDetails
+
   const onDeleteItem = () => {
     onDeleteHistory(id)
   }
+
   return (
     <li className="history">
-      <p className="time">{timeAccessed}</p>
-      <div className="domain-details-container">
-        <img src={logoUrl} alt="domain logo" className="domain-logo" />
-        <p className="title">{title}</p>
-        <p className="domain-url">{domainUrl}</p>
-      </div>
-      <div className="button-container">
-        <button className="delete-button" onClick={onDeleteItem} type="button">
+      <p className="time-stamp">{timeAccessed}</p>
+      <div className="content">
+        <div className="domain-details-container">
+          <img src={logoUrl} alt="domain logo" className="domain-logo" />
+          <div className="domain-content">
+            <p className="title">{title}</p>
+            <p className="domain-url">{domainUrl}</p>
+          </div>
+        </div>
+        <button
+          type="button"
+          data-testid="delete"
+          className="delete-button"
+          onClick={onDeleteItem}
+        >
           <img
             src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
             alt="delete"
